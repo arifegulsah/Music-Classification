959f3e3590a9937ecc7bd44222158bf86f67d0ed
Gülşah <arifegulsaherol@gmail.com>
2022-05-14 22:25:22 +0300

    importing lib düzeltildi

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ for i in range(1, 21):
 header += ' label'
 header = header.split()
 
-csvfilepath = 'C:/Users/arife/Desktop/datasesdeneme.csv'
+csvfilepath = 'csvfile/audioDatas.csv'
 
 file = open(csvfilepath, 'w', newline='')
 with file:
@@ -33,8 +33,8 @@ with file:
     writer.writerow(header)
 genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
 for g in genres:
-    for filename in os.listdir(f'C:/Users/arife/Desktop/genres/{g}'):
-        songname = f'C:/Users/arife/Desktop/genres/{g}/{filename}'
+    for filename in os.listdir(f'genres/{g}'):
+        songname = f'genres/{g}/{filename}'
         y, sr = librosa.load(songname, mono=True, duration=30)
         chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
         rmse = librosa.feature.rms(y=y)
@@ -53,35 +53,30 @@ for g in genres:
             writer.writerow(to_append.split())
             
 
-
 data = pd.read_csv(csvfilepath)
 data.head()
-
 
 # Dropping unneccesary columns
 data = data.drop(['filename'],axis=1)
 data.head()
-
 
 genre_list = data.iloc[:, -1]
 encoder = LabelEncoder()
 y = encoder.fit_transform(genre_list)
 print(y)
 
-
-
 scaler = StandardScaler()
 X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype = float))
-
-
-
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 
+from tensorflow import keras 
+from tensorflow.keras import Sequential
+from tensorflow.keras import models
+from tensorflow.keras import layers
 
-from keras import models
-from keras import layers
+
 model = models.Sequential()
 model.add(layers.Dense(256, activation='relu', input_shape=(X_train.shape[1],)))
 model.add(layers.Dense(128, activation='relu'))
@@ -94,7 +89,7 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 
-
+model.summary()
 
 
 history = model.fit(X_train,
@@ -107,10 +102,33 @@ test_loss, test_acc = model.evaluate(X_test,y_test)
 print('test_acc: ',test_acc)
 
 
+
 predictions = model.predict(X_test)
 np.argmax(predictions[2])
 
+deneme = []
+for element in predictions:
+    temp = np.argmax(element)
+    deneme.append(temp)
+    
+del deneme['Type']
+
+deneme.head()
+data = data.drop(['filename'],axis=1)
+  
+
+from sklearn.model_selection import cross_val_predict
+y_pred = cross_val_predict(model, X_train, y_train)
 
 
+import numpy as np
+a = np.unique(y)
+print(a)
 
+target_names = ['0blues', '1classical', '2country', '3disco', '4hiphop', '5jazz', '6metal', '7pop', '8reggae', '9rock']
+
+from sklearn.metrics import classification_report
+classification_report(y_test, deneme[2].astype(int), target_names=target_names)
+
+print(type(deneme[2]))
 
